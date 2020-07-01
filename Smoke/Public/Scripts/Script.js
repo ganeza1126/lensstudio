11938be3a44760fd4b72d464490d96993f43f231
ganeza1126 <ganeza1126@g.ecc.u-tokyo.ac.jp>
2020-07-01 20:00:53 +0900

    add smoke

--- Smoke/Public/Scripts/Script.js
+++ Smoke/Public/Scripts/Script.js
@@ -1,14 +1,29 @@
 // -----JS CODE-----
-//@input Component.Camera camera
+//input Component.Camera camera
 //@input Asset.ObjectPrefab myPrefab
-//@input SceneObject obj
+//input SceneObject obj
+//@input Asset.Texture deviceCameraTexture
+//@input Component.Text textcomponent
 
+
+var deviceSize = new vec2(script.deviceCameraTexture.getWidth(), script.deviceCameraTexture.getHeight());
+
+print(deviceSize);
 //var instanceobj = script.obj.copyComponent(script.getSceneObject());
+var wid = 30;
+var hig = 20;
+var sc = 30;
+var scale = new vec3(sc, sc, sc);
 
-for (var i = 0; i < 3; i++) {
+for (var i = 0; i < 50; i++) {
 	var instanceObject = script.myPrefab.instantiate(script.getSceneObject());
-	var worldPosition = new vec3(Math.random() * 20, 0, 0);
+	var worldPosition = new vec3((Math.random() - 0.5) * wid, (Math.random() - 0.5) * hig, 0);
+	var radians = Math.random() * Math.PI * 2;
+	var axis = new vec3(0, 0, 1);
+	var randrot = quat.angleAxis(radians, axis);
 	instanceObject.getTransform().setWorldPosition(worldPosition);
+	instanceObject.getTransform().setLocalScale(scale);
+	instanceObject.getTransform().setLocalRotation(randrot);
 }
 
 /*
